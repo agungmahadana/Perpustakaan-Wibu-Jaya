f22858909c52f398f34dc531c998d1eb698afa9f
Agung Mahadana <100138244+agungmahadana@users.noreply.github.com>
2023-01-21 17:32:29 +0800

    fix page files name

---
+++ Dashboard_🎛️.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(page_title="Perpustakaan Wibu Jaya", page_icon=":books:")
+st.title("Welcome to Perpustakaan Wibu Jaya! 👋")
+st.markdown("""
+Perpustakaan Wibu Jaya adalah apliaksi perpustakaan sederhana yang dibuat oleh **KELOMPOK 4** menggunakan Python, MySQL, dan Streamlit. Aplikasi ini dipergunakan untuk memenuhi tugas akhir mata kuliah Desain dan Analisis Algoritma, yang diampu oleh Bapak I Gusti Agung Gede Arya Kadyanan, S.Kom., M.Kom.
+### Fitur yang tersedia
+- Menampilkan daftar buku
+- Meminjam buku
+- Informasi seputar kami
+""")
